1cb43ef01c70fb0309f2572ef2ce9c96e5624219
edgarxongas <edgaroboe@outlook.com>
2021-08-07 17:42:47 -0300

    litle fix on menu

--- src/pages/Home/MenuSection/index.tsx
+++ src/pages/Home/MenuSection/index.tsx
@@ -15,7 +15,7 @@ export function MenuSection({ title, menu, foodType }:MenuSectionProps){
     <section>
       <h1>{title}</h1>
       <div className="menu-section">
-        { menu.length > 0 && menu.map(item=>item.type===foodType && <MenuItem item={item} key={item.id}/>)}
+        { menu.length > 0 && menu.map(item=>item.type===foodType && item.isAvailable && <MenuItem item={item} key={item.id}/>)}
         { menu.length === 0 && <Placeholder/>}
       </div>
     </section>
