d3d8c283b371351dd614c32e3e9243a508f300aa
edgarxongas <edgaroboe@outlook.com>
2021-08-24 20:46:34 -0300

    changed skeleton-loading

--- src/components/OrdersList/Placeholder/index.tsx
+++ src/components/OrdersList/Placeholder/index.tsx
@@ -2,9 +2,27 @@
 export function OrderPlaceholder(){
   return(
     <>
-      <li id="order-placeholder"></li>
-      <li id="order-placeholder"></li>
-      <li id="order-placeholder"></li>
+      <li id="order-placeholder">
+        <div className="skeleton" />
+        <div className="skeleton" />
+        <div className="skeleton" />
+      </li>
+      <li id="order-placeholder">
+        <div className="skeleton" />
+        <div className="skeleton" />
+        <div className="skeleton" />
+      </li>
+      <li id="order-placeholder">
+        <div className="skeleton" />
+        <div className="skeleton" />
+        <div className="skeleton" />
+      </li>
     </>
   )
 }
+
+/* Alternativa de placeholder com #order-placeholder
+<li id="order-placeholder" />
+<li id="order-placeholder" />
+<li id="order-placeholder" /> 
+*/
